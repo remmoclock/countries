fb06efdaf796c68f6e71bd14ed069e78968b9f40
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-06-11 18:35:23 +0200

    fetch data axios

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
-  return <h1 className="text-3xl font-bold text-red-900">World Flags</h1>;
+  const [data, setData] = useState([]);
+
+  const flags = data.map((d) => {
+    return <h1 className="text-3xl font-bold text-red-900">{d.flag}</h1>;
+  });
+
+  const fetchData = () => {
+    axios.get(`https://restcountries.com/v3.1/all`).then((res) => {
+      const data = res.data;
+      console.log("data", data);
+      setData(data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-900">World Flags</h1>
+      {flags}
+    </div>
+  );
 }
 
 export default Home;
