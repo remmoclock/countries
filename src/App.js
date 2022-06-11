cb05bd42ebf67fa9344f628db6d8c0dc2eb2cc39
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-06-11 17:25:34 +0200

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App bg-orange-200 h-screen">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <h1 className="text-3xl font-bold text-red-900">World Flags</h1>;
+}
+
+export default Home;
