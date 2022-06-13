a2d426b1a38c975130cef978e2f5a0752d74b788
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-06-13 19:30:25 +0200

    create Loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Loader from "./components/Loader/Loader";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App bg-orange-200 overflow-auto h-screen ">
       <Home />
+      <Loader />
     </div>
   );
 }
---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div>
+      <img src={process.env.PUBLIC_URL + "/worldLoader.gif"} alt="" />
+    </div>
+  );
+}
+
+export default Loader;
