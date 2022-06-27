4cfa7412a520e9bfe033e770a542ad2c450a278e
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-06-28 00:40:40 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App bg-black overflow-auto w-screen h-screen ">
+    <div className="App bg-black overflow-auto w-full h-screen ">
       <Home />
     </div>
   );
